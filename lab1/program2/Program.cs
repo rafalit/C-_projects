4bdc681db5d7823db700e5b3096b577b2bca03d7
Rafał Szarowicz <93370514+rafalit@users.noreply.github.com>
2024-03-08 16:12:54 +0100

    checking if a word contaons another word

---
+++ lab1/program2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Funkcja("jed", "plik");
+    }
+
+    static void Funkcja(string szukany_wyraz, string wyraz)
+    {
+        using (StreamReader sr = new StreamReader($"{wyraz}.txt"))
+        {
+            int numerLinii = 0;
+
+            while (!sr.EndOfStream)
+            {
+                numerLinii++;
+                string napis = sr.ReadLine();
+
+                if (napis.Contains(szukany_wyraz))
+                {
+                    int index = napis.IndexOf(szukany_wyraz);
+                    Console.WriteLine($"linijka {numerLinii}, pozycja: {index}");
+                }
+            }
+        }
+    }
+}

3da3ae5593e196a8e996ebdfe6a74e569fbd4014
Rafał Szarowicz <93370514+rafalit@users.noreply.github.com>
2024-03-08 16:14:39 +0100

    writing to file, random, seed, passed by args

---
+++ lab1/program3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Funkcja(string nazwa, int ilosc, int min, int max, int seed, bool czy_rzecz)
+    {
+        using (StreamWriter sw = new StreamWriter($"{nazwa}.txt"))
+        {
+            Random random = new Random(seed);
+
+            for (int i = 0; i < ilosc; i++)
+            {
+                double losowaLiczba;
+
+                if (czy_rzecz)
+                {
+                    losowaLiczba = random.NextDouble() * (max - min) + min;
+                }
+                else
+                {
+                    losowaLiczba = random.Next(min, max + 1);
+                }
+                sw.WriteLine(losowaLiczba);
+            }
+
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 6)
+        {
+            Console.WriteLine("Nieprawidłowa liczba argumentów.");
+            return;
+        }
+
+        string n = args[0];
+        int i = int.Parse(args[1]);
+        int x = int.Parse(args[2]);
+        int y = int.Parse(args[3]);
+        int s = int.Parse(args[4]);
+        bool f = bool.Parse(args[5]);
+
+        Funkcja(n, i, x, y, s, f);
+    }
+}

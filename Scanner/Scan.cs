4fe23502eb6a45416474ba76346a36b20a566bfe
Rafał Szarowicz <93370514+rafalit@users.noreply.github.com>
2024-03-14 00:15:05 +0100

    done, scanner with errors and compatibility

--- Scanner/Scan.cs
+++ Scanner/Scan.cs
@@ -1,3 +1,4 @@
+﻿
 using System;
 
 class Program
@@ -19,41 +20,124 @@ class Program
 
     class Token
     {
-        public TokenType Code {get;}
-        public string Value {get;}
+        public TokenType Code { get; }
+        public string Value { get; }
 
         public Token(TokenType c, string v)
         {
-            Code=c;
-            Value=v;
+            Code = c;
+            Value = v;
         }
     }
 
-    class TokenScanner(string input)
-    {   
+    class TokenScanner
+    {
         private readonly string input;
         private int position;
 
         public TokenScanner(string input)
         {
-            this.input=input;
-            position=0;
+            this.input = input;
+            position = 0;
         }
 
         public Token GetNextToken()
         {
             SkipWhiteSpace();
-            
-            if(position >= input.Length)
-                return new Token(TokenType.EndOfFile, "");
-            
 
+            if (position >= input.Length)
+                return new Token(TokenType.EndOfFile, "");
+
+            char currentChar = input[position];
+
+            if (char.IsDigit(currentChar))
+            {
+                return ScanInteger();
+            }
+            else if (char.IsLetter(currentChar))
+            {
+                return ScanIdentifier();
+            }
+            else
+            {
+                position++;
+                switch (currentChar)
+                {
+                    case '+':
+                        return new Token(TokenType.Plus, "+");
+                    case '-':
+                        return new Token(TokenType.Minus, "-");
+                    case '*':
+                        return new Token(TokenType.Multiply, "*");
+                    case '/':
+                        return new Token(TokenType.Divide, "/");
+                    case '(':
+                        return new Token(TokenType.LeftParenthesis, "(");
+                    case ')':
+                        return new Token(TokenType.RightParenthesis, ")");
+                    default:
+                        ThrowError($"Nieznany token {currentChar}");
+                        return new Token(TokenType.Error, "");
+                }
+            }
+        }
+
+        private Token ScanInteger()
+        {
+            int start = position;
+            while (position < input.Length && char.IsDigit(input[position]))
+            {
+                position++;
+            }
+            return new Token(TokenType.Integer, input.Substring(start, position - start));
+        }
+
+        private Token ScanIdentifier()
+        {
+            int start = position;
+            while (position < input.Length && (char.IsLetterOrDigit(input[position]) || input[position] == '_'))
+            {
+                position++;
+            }
+            return new Token(TokenType.Identifier, input.Substring(start, position - start));
+        }
+
+        private void SkipWhiteSpace()
+        {
+            while (position < input.Length && char.IsWhiteSpace(input[position]))
+            {
+                position++;
+            }
+        }
+
+        private void ThrowError(string message)
+        {
+            throw new Exception($"{message}, (kolumna {position + 1})");
         }
     }
 
     public static void Main()
     {
+        Console.WriteLine("Podaj wyrażenie matematyczne:");
+        string input = Console.ReadLine();
 
+        try
+        {
+            TokenScanner scanner = new TokenScanner(input);
+            Token token;
 
+            do
+            {
+                token = scanner.GetNextToken();
+                Console.WriteLine($"({token.Code}, {token.Value})");
+            } while (token.Code != TokenType.EndOfFile);
+
+            Console.WriteLine("Skanowanie zakończone.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Błąd skanera: {ex.Message}");
+        }
     }
+
 }

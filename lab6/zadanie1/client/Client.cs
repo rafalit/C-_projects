45b653291511dcbfafe7bf2e8467901e03d7b764
rafalit <szarowiczrafal@gmail.com>
2024-04-12 15:59:58 +0200

    client->server

---
+++ lab6/zadanie1/client/Client.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPHostEntry host = Dns.GetHostEntry("localhost");
+IPAddress ipAddress = host.AddressList[0];
+IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+Socket socket = new (
+    localEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+);
+
+socket.Connect(localEndPoint);
+string message = "Message from client!";
+byte[] messageByte = Encoding.UTF8.GetBytes(message);
+socket.Send(messageByte, SocketFlags.None);
+
+var bufor = new byte[1_024];
+int byteNumbers = socket.Receive(bufor, SocketFlags.None);
+
+Console.WriteLine("Server's response:");
+try{
+    socket.Shutdown(SocketShutdown.Both);
+    socket.Close();
+}
+catch{}
+
---
+++ lab6/zadanie1/server/Server.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPHostEntry host = Dns.GetHostEntry("localhost");
+IPAddress ipAddress = host.AddressList[0];
+IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+Socket serverSocket = new(
+    localEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp
+);
+
+serverSocket.Bind(localEndPoint);
+serverSocket.Listen(100);
+
+Socket clientSocket = serverSocket.Accept();
+byte[] bufor = new byte[1_024];
+int received = clientSocket.Receive(bufor, SocketFlags.None);
+string clientMessage = Encoding.UTF8.GetString(bufor, 0, received);
+Console.WriteLine("Client's message: ");
+string response = "Server's response";
+var echoBytes = Encoding.UTF8.GetBytes(response);
+clientSocket.Send(echoBytes, 0);
+try{
+    serverSocket.Shutdown(SocketShutdown.Both);
+    serverSocket.Close();
+}
+catch{}
